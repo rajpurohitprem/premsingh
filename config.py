f98ebbb4bb8ecbc82e4f4ea8c6e8792a803fc750
rajpurohitprem <purohitpremsingh2005@gmail.com>
2025-07-16 15:36:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -63,8 +63,11 @@ async def load_or_prompt_config():
     #await client.start(phone=config["phone"])
 
 
-        print("👉 Full config edit selected.")
-        print("API ID: "api_id"")
+        print("👉 Full config done.")
+        print("📌 Your API ID is:", config["api_id"])
+        print("📌 Your API ID is:", config["api_hash"])
+        print("📌 Your API ID is:", config["api_phone"])
+
         
 
     return config, client
